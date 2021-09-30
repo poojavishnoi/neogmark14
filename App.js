0d8eb557dedecf3febd5b2944067b42e1ed4887d
Pooja Vishnoi <pooja.k.vishnoi@gmail.com>
2021-09-30 15:11:55 +0530

    Handled negative input by using an alert message

--- App.js
+++ App.js
@@ -16,20 +16,26 @@ function clickHandler() {
 }
 
 function calculateProfitOrLoss(initial, current, number) {
-  if(initial > current){
-    let loss = (initial-current) * number;
-    let lossPercentage = (loss / (initial*number)) * 100;
-    ouptputEl.style.color = "red";
-    ouptputEl.innerHTML = `Whoops!! Your loss is ${loss} and loss Percentage is ${lossPercentage}% :(`;
-  }
-  else if(initial < current){
-    let profit = (current-initial) * number;
-    let profitPercentage = (profit / (initial*number)) * 100;
-    ouptputEl.style.color = "green";
-    ouptputEl.innerHTML = `Yay!! Your Profit is ${profit.toFixed(2)} and profit Percentage is ${profitPercentage.toFixed(2)}%`;
-  } 
-  else {
-    ouptputEl.style.color = "white";
-    ouptputEl.innerHTML = `No pain No gain, and no gain no pain :)`;
+
+  if (initial < 0 || current < 0 || number < 0){
+    alert("The number entered cannot be negative..")
+  } else {
+    
+    if(initial > current){
+      let loss = (initial-current) * number;
+      let lossPercentage = (loss / (initial*number)) * 100;
+      ouptputEl.style.color = "red";
+      ouptputEl.innerHTML = `Whoops!! Your loss is ${loss} and loss Percentage is ${lossPercentage}% :(`;
+    }
+    else if(initial < current){
+      let profit = (current-initial) * number;
+      let profitPercentage = (profit / (initial*number)) * 100;
+      ouptputEl.style.color = "green";
+      ouptputEl.innerHTML = `Yay!! Your Profit is ${profit.toFixed(2)} and profit Percentage is ${profitPercentage.toFixed(2)}%`;
+    } 
+    else {
+      ouptputEl.style.color = "white";
+      ouptputEl.innerHTML = `No pain No gain, and no gain no pain :)`;
+    }
   }
 }
